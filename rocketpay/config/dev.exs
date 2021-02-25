8c0a916e6d40b33f9e7a4c8c26f7a37c9434d961
gabrielgsv <gabriel.soaresv@orgsystem.com.br>
2021-02-25 02:26:42 -0300

    class frist

--- rocketpay/config/dev.exs
+++ rocketpay/config/dev.exs
@@ -4,7 +4,7 @@ use Mix.Config
 config :rocketpay, Rocketpay.Repo,
   username: "postgres",
   password: "postgres",
-  database: "rocketpay_dev",
+  database: "postgres",
   hostname: "localhost",
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
